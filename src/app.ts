d4b835d3868ffad7202e50efce46fa6830e6d9f7
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2020-09-02 10:05:33 +0200

    basic version of select & share app

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+//ℹ NOTE edit api key before you start
+const GOOGLE_API_KEY = 'API_KEY_HERE';
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress,
+      )}&key=${GOOGLE_API_KEY}`,
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+
+      const map = new google.maps.Map(
+        document.getElementById('map') as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        },
+      );
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
